8ae41907c1fa87e578f4fbce025ba994f959e3a6
Soufiane El Barji <soufiane.elbarji2004@gmail.com>
2025-07-01 15:26:53 +0100

    Last Update

---
+++ Speech_To_img/Imagen.py
@@ -0,0 +1,39 @@
+from google import genai
+from google.genai import types
+from PIL import Image
+from io import BytesIO
+import os
+
+GEMINI_API_KEY = ""
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+# keyword = input("Enter your prompt: ")
+
+def Imagen(keyword):
+
+    response = client.models.generate_images(
+        model='imagen-3.0-generate-002',
+        prompt=f"A {keyword} in Cartoon style inspired by early 2010s Western animation, featuring thick black outlines, flat cel-shading, bold and simple color palette, soft lighting. Clean digital 2D vector look, minimal detail, no texture, no gradients, no realism. TV animation aesthetic.",
+        config=types.GenerateImagesConfig(
+            number_of_images=1,
+        )
+    )
+
+    output_dir = "Speech_To_img/Images"
+    os.makedirs(output_dir, exist_ok=True)
+
+    for i, generated_image in enumerate(response.generated_images):
+
+        base_filename = "image"
+        extension = ".png"
+        counter = 1
+        
+        while True:
+            filename = os.path.join(output_dir, f"{base_filename}_{counter}{extension}")
+            if not os.path.exists(filename):
+                break 
+        
+            counter += 1
+                
+        generated_image.image.save(filename)
+        print(f"Image saved to: {filename}")
--- Speech_To_img/main.py
+++ Speech_To_img/main.py
@@ -3,8 +3,9 @@ from PIL import Image
 from pyparsing import Keyword
 import speech_recognition as sr
 import re
+import Imagen
 
-HF_TOKEN = ""
+# HF_TOKEN = ""
 
 # ── VOICE AND AI FUNCTIONS ─────────────────────
 def listen():
@@ -27,16 +28,16 @@ def extract_keyword(cmd):
     m = re.search(r"about\s+(.+)$", cmd)
     return (m.group(1) if m else cmd).strip()
 
-def generate_image(keyword):
+# def generate_image(keyword):
 
-    client = InferenceClient(
-        model="stabilityai/stable-diffusion-xl-base-1.0",
-        token=HF_TOKEN
-    )
-    prompt = f"Clean line and Realistic art of {keyword}. Pure outline illustration, white background; Stroke-based art style; Minimal detail; crisp lines; line art only; no fill; no shading; no color; no background; no texture; no gradients; no patterns; no noise; no artifacts; no distortion; no blurriness; no smudging; no imperfections; no errors; no mistakes; no flaws; no defects; no blemishes; no scratches; no scuffs; no marks; no stains; no dirt; no dust; no fingerprints; no smudges; no smears."
-    image = client.text_to_image(prompt, guidance_scale=15)
-    image.save("img.png")
-    print("Image saved as img.png")
+#     client = InferenceClient(
+#         model="stabilityai/stable-diffusion-xl-base-1.0",
+#         token=HF_TOKEN
+#     )
+#     prompt = f"Clean line and Realistic art of {keyword}. Pure outline illustration, white background; Stroke-based art style; Minimal detail; crisp lines; line art only; no fill; no shading; no color; no background; no texture; no gradients; no patterns; no noise; no artifacts; no distortion; no blurriness; no smudging; no imperfections; no errors; no mistakes; no flaws; no defects; no blemishes; no scratches; no scuffs; no marks; no stains; no dirt; no dust; no fingerprints; no smudges; no smears."
+#     image = client.text_to_image(prompt, guidance_scale=15)
+#     image.save("img.png")
+#     print("Image saved as img.png")
 
 # ── MAIN FUNCTION ────────────────────────────
 def main():
@@ -47,7 +48,9 @@ def main():
     keyword = extract_keyword(cmd)
     print("Keyword:", keyword)
 
-    generate_image(keyword)
+    # generate_image(keyword)
+
+    Imagen.Imagen(keyword)
 
 # ── MAIN EXECUTION ───────────────────────────
 if __name__ == "__main__":
