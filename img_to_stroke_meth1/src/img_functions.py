4693e4453364f58fcf9ce1bff10d1f8b8ed1b469
ASTAgold <abde2006llah@gmail.com>
2025-07-03 23:44:11 +0100

    cleaned outline to path conversion code (put into function in img_function.py)

--- img_to_stroke_meth1/src/img_functions.py
+++ img_to_stroke_meth1/src/img_functions.py
@@ -1,5 +1,13 @@
 import numpy as np
 import cv2 as cv
+import subprocess
+from svgpathtools import svg2paths, svg2paths2
+from simplification.cutil import simplify_coords
+
+
+POTRACE_PATH = "../potrace-1.16.win64/potrace"
+tolerance = 2  # Higher = more aggressive simplification (in pixels or mm)
+
 
 def remap(value, inputStart, inputEnd, outputStart, outputEnd):
     result = (value - inputStart)/(inputEnd - inputStart)*(outputEnd - outputStart) + outputStart
@@ -23,6 +31,76 @@ def generate_outline(img):
     edges = cv.Canny(img_resize,100,200)
     return edges
 
-#
-# def generate_contours(outline):
-#     contours, _ = cv.findContours(outline, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+def generate_paths(edges):
+    edges_invert = cv.bitwise_not(edges)
+
+    cv.imwrite('temp/edge.bmp', edges_invert)
+
+    # Convert BMP to PBM
+    img = "temp/edge.bmp"
+    potrace = POTRACE_PATH
+
+    # Run potrace to get SVG
+    subprocess.run([potrace, img, "-s", "-o", "temp/svg_outline.svg"])
+
+
+    # svg to path
+    paths, attributes, svg_attributes  = svg2paths2("temp/svg_outline.svg")
+
+    # Find bounds
+    all_x = []
+    all_y = []
+    height, width = edges.shape[:2]
+    aspect_ratio = width / height
+
+    for path in paths:
+        for seg in path:
+            min_x, max_x, min_y, max_y = seg.bbox()
+            all_x.append(min_x)
+            all_x.append(max_x)
+            all_y.append(min_y)
+            all_y.append(max_y)
+
+    min_x, max_x = min(all_x), max(all_x)
+    min_y, max_y = min(all_y), max(all_y)
+
+    if max_x - min_x < max_y - min_y:
+        min_y -= height*0.2
+        max_y += height*0.2
+        centre = (max_x + min_x)/2
+        new_width = (max_y - min_y) * aspect_ratio
+        min_x = centre - new_width/2
+        max_x = centre + new_width/2
+    else:
+        min_x -= width*0.2
+        max_x += width*0.2
+        centre = (max_y + min_y)/2
+        new_height = (max_x - min_x) * aspect_ratio
+        min_y = centre - new_height/2
+        max_y = centre + new_height/2
+
+
+    all_strokes = []  
+    density = 20 
+    # Flatten into points
+    for path in paths:
+        stroke_points = []
+        for segment in path:
+            for t in [i/density for i in range(density-1)]:  # Sample the path
+                point = segment.point(t)
+                x, y = point.real, point.imag
+                # print(f"x : {x} , y : {y}")
+                new_x = int(remap(x, min_x, max_x, 0, width))
+                new_y = int(remap(y, min_y, max_y, 0, height))
+
+                stroke_points.append((new_x, new_y))
+        all_strokes.append(stroke_points)
+
+    simplified_strokes = []
+
+    for stroke in all_strokes:
+        # RDP requires a flat list of [x, y] pairs
+        simplified = simplify_coords(stroke, tolerance)
+        simplified_strokes.append(simplified)
+
+    return simplified_strokes
--- img_to_stroke_meth1/src/main2.py
+++ img_to_stroke_meth1/src/main2.py
@@ -6,11 +6,11 @@ from svgpathtools import svg2paths, svg2paths2
 from matplotlib import pyplot as plt
 from PIL import Image
 import subprocess
-from rdp import rdp
+from simplification.cutil import simplify_coords
 
  
 # import image
-img_origine = cv.imread('samples/image_4.png')
+img_origine = cv.imread('samples/darwin.jpg')
 assert img_origine is not None, "file could not be read, check with os.path.exists()"
 
 # gray :P
@@ -18,129 +18,20 @@ img_gray = cv.cvtColor(img_origine, cv.COLOR_BGR2GRAY)
 
 # generate outline
 edges = img_func.generate_outline(img_origine)
-edges_invert = cv.bitwise_not(edges)
 
-cv.imwrite('temp/edge.bmp', edges_invert)
+paths = img_func.generate_paths(edges)
+print(paths)
 
-# Convert BMP to PBM
-img = "temp/edge.bmp"
-potrace = "../potrace-1.16.win64/potrace"
+# ### PLOTING PATHS COMPARISON
 
-# Run potrace to get SVG
-subprocess.run([potrace, img, "-s", "-o", "temp/svg_outline.svg"])
-
-
-# svg to path
-paths, attributes, svg_attributes  = svg2paths2("temp/svg_outline.svg")
-
-# Find bounds
-all_x = []
-all_y = []
-height, width = edges.shape[:2]
-aspect_ratio = width / height
+plt.figure(figsize=(6, 6))
+plt.title("Simplified Stroke Points")
 
 for path in paths:
-    for seg in path:
-        min_x, max_x, min_y, max_y = seg.bbox()
-        all_x.append(min_x)
-        all_x.append(max_x)
-        all_y.append(min_y)
-        all_y.append(max_y)
+    x_vals = [p[0] for p in path]
+    y_vals = [p[1] for p in path]
+    plt.plot(x_vals, y_vals)  # s = point size
 
-min_x, max_x = min(all_x), max(all_x)
-min_y, max_y = min(all_y), max(all_y)
-
-if max_x - min_x < max_y - min_y:
-    min_y -= height*0.1
-    max_y += height*0.1
-    centre = (max_x + min_x)/2
-    new_width = (max_y - min_y) * aspect_ratio
-    min_x = centre - new_width/2
-    max_x = centre + new_width/2
-else:
-    min_x -= width*0.1
-    max_x += width*0.1
-    centre = (max_y + min_y)/2
-    new_width = (max_x - min_x) * aspect_ratio
-    min_y = centre - new_width/2
-    max_y = centre + new_width/2
-
-
-point_image_svg = np.zeros((edges.shape[0], edges.shape[1], 3), dtype=np.uint8)
-all_strokes = []  
-density = 4
-# Flatten into points
-for path in paths:
-    stroke_points = []
-    rnd_color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-    for segment in path:
-        for t in [i/density for i in range(density-1)]:  # Sample the path
-            point = segment.point(t)
-            x, y = point.real, point.imag
-            # print(f"x : {x} , y : {y}")
-            new_x = int(img_func.remap(x, min_x, max_x, 0, width))
-            new_y = int(img_func.remap(y, min_y, max_y, 0, height))
-
-            stroke_points.append((new_x, new_y))
-            cv.circle(point_image_svg, (new_x, new_y), radius=1, color=rnd_color, thickness=-1)
-            # Now x, y is one coordinate the robot should go to
-
-    all_strokes.append(stroke_points)
-
-point_image_svg = cv.flip(point_image_svg, 0)
-cv.imshow("svg paths", point_image_svg)
-
-cv.imwrite('temp/paths.jpg', point_image_svg)
-
-
-
-
-# for stroke in all_strokes:
-#     print(len(stroke))
-
-
-# generate svg form outline (bitmape tarcing)
-
-
-# # plot image
-# plt.subplot(1,3,1)
-# plt.imshow(img_origine)
-
-# #plot contour
-# plt.subplot(1,3,2)
-# plt.imshow(edges_invert, cmap='gray')
-
-# # plot contours (colored)
-# plt.subplot(1,3,3)
-# plt.imshow(point_image)
-
-# plt.show()
-cv.waitKey(0)
-
-
-
-
-
-
-
-
-
-# generate contours
-# contours, _ = cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-
-
-# ### display contours
-# # Create a 3-channel color image
-# point_image = np.zeros((edges.shape[0], edges.shape[1], 3), dtype=np.uint8)
-
-# # Draw each point
-# for contour in contours:
-#     rnd_color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-#     # print(str(len(contour)))
-#     # k = cv.waitKey(0) # Wait for a keystroke in the window
-#     # cv.imshow("Contour Points", point_image)
-#     for point in contour:
-#         x, y = point[0]  # shape is (1, 2)
-#         cv.circle(point_image, (x, y), radius=1, color=rnd_color, thickness=-1)
-# cv.putText(point_image, "stroke number : " + str(len(contours)), (10,20), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
-# print("stroke number :" + str(len(contours)))
+plt.gca().set_aspect('equal')
+plt.grid(True)
+plt.show()
