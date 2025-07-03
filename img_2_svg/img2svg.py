54bf915c8f3ed8ef7510bff56df9fd9674e64b52
Soufiane El Barji <soufiane.elbarji2004@gmail.com>
2025-07-03 16:03:44 +0100

    optimization of the svg files

--- img_2_svg/img2svg.py
+++
@@ -1,7 +0,0 @@
-import aspose.words as aw
-
-doc = aw.Document()
-builder = aw.DocumentBuilder(doc)
-
-shape = builder.insert_image("colored_contours5.jpg")
-shape.get_shape_renderer().save("output.svg", aw.saving.ImageSaveOptions(aw.SaveFormat.SVG))
